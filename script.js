7c2fd51834d1a1a77e9ee2dc767e2c1740246109
DenziLOL <doshikdog1@gmail.com>
2024-12-07 03:47:47 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const congrats = document.getElementById('congrats');
 const buttons = document.getElementById('buttons');
 
 passwordInput.addEventListener('input', function() {
-    if (passwordInput.value === '8969r') {
+    if (passwordInput.value === 'Sex') {
         congrats.style.display = 'block';
         buttons.style.display = 'flex';
         passwordInput.style.display = 'none';
